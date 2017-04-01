bdf2e526c1846f7383d0b0ecf9a7be64a283bacd
alex_r <blank.basic@gmail.com>
2017-04-01 09:30:46 +0300

    Fix typo

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,17 +1,14 @@
 def sort(arr: list) -> list:
-    """Bubble sort with small optimisations:
+    """Bubble sort with small optimizations:
     Time complexity O(n**2), Memory O(1), Stable
     """
     L = len(arr)
     for i in range(L):
-
         swapped = False
         for j in range(1, L - i):
             if arr[j] < arr[j - 1]:
                 arr[j - 1], arr[j] = arr[j], arr[j - 1]
                 swapped = True
-
         if not swapped:
             break
-
     return arr
