bb925c07dc74a253be672bf294ea0be1895f0386
alex_r <blank.basic@gmail.com>
2017-04-02 22:58:08 +0300

    Add temporary stub

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,14 +1,2 @@
-def sort(arr: list) -> list:
-    """Bubble sort with small optimizations:
-    Time complexity O(n**2), Memory O(1), Stable
-    """
-    L = len(arr)
-    for i in range(L):
-        swapped = False
-        for j in range(1, L - i):
-            if arr[j] < arr[j - 1]:
-                arr[j - 1], arr[j] = arr[j], arr[j - 1]
-                swapped = True
-        if not swapped:
-            break
-    return arr[-1]
+def sort(arr):
+    pass
