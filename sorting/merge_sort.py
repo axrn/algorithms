8a335870c74117ed279b40d3f558a3b1d1efa226
alex_r <blank.basic@gmail.com>
2017-04-04 19:05:41 +0300

    Add merge sort recursive implementation

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,2 +1,27 @@
-def sort(arr):
-    pass
+def sort(arr: list) -> list:
+    """Merge sort recursive implementation:
+    Time complexity O(n log(n)), Memory O(n), Stable
+    """
+    if len(arr) <= 1:
+        return arr
+
+    middle = int(len(arr) / 2)
+    left = sort(arr[:middle])
+    right = sort(arr[middle:])
+    return merge(left, right)
+
+
+def merge(left_arr: list, right_arr: list) -> list:
+    result = []
+    li, ri = 0, 0
+    while li < len(left_arr) and ri < len(right_arr):
+        if left_arr[li] <= right_arr[ri]:
+            result.append(left_arr[li])
+            li += 1
+        else:
+            result.append(right_arr[ri])
+            ri += 1
+
+    result.extend(left_arr[li:])
+    result.extend(right_arr[ri:])
+    return result
