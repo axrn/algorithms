99ee7238b8b7ba23802891b03449e422f6c7640f
alex_r <blank.basic@gmail.com>
2017-05-18 15:05:05 +0300

    Init #2 Splay BST

--- data structures/BST Splay.py
+++ data structures/BST Splay.py
@@ -1,14 +1,14 @@
 import sys
 
+
 class Node:
-    __slots__ = ["value", "parent", "left", "right", "height"]
+    __slots__ = ["value", "parent", "left", "right"]
 
     def __init__(self, value: int) -> None:
         self.value = value
         self.parent = None
         self.left = None
         self.right = None
-        self.height = 1
 
     def __lt__(self, other: 'Node') -> bool:
         return self.value < other.value
@@ -22,7 +22,12 @@ class Node:
         return self.value == other.value
 
     def __repr__(self) -> str:
-        return "{}|p{}".format(self.value, self.parent.value if self.parent else None)
+        r_or_l = ""
+        p = None
+        if self.parent:
+            p = self.parent.value
+            r_or_l = "lc" if self == self.parent.left else "rc"
+        return "{}|{}{}".format(self.value, r_or_l, p)
 
 
 class BST:
@@ -32,15 +37,15 @@ class BST:
     def __repr__(self):
         result = """
                                {}
-                          /             \\
-                   {}                {}
-                 /      \\               /      \\
-             {}       {}          {}      {}
+                          /              \\
+                   {}                 {}
+                 /      \\                /      \\
+             {}      {}         {}    {}
             /    \\      /   \\         /   \\      /   \\
-          {}  {} {}  {}   {}  {} {}  {}
+        {}  {} {} {}  {}  {} {} {}
 
         """
-        EMPTY = "    "
+        EMPTY = "     "
         all_nodes = []
         this_level = [self.root]
         while any([x for x in this_level if x != EMPTY]):
@@ -50,7 +55,7 @@ class BST:
                 next_level.append(n.left if n != EMPTY and n.left else EMPTY)
                 next_level.append(n.right if n != EMPTY and n.right else EMPTY)
             this_level = next_level
-        if len(all_nodes) == 31: result += "{} " * 16
+        if len(all_nodes) == 31: result += "{}" * 16
         if len(all_nodes) == 7: result = result[:220]
         if len(all_nodes) == 3: result = result[:120]
         if len(all_nodes) == 1: result = result[:80]
@@ -82,8 +87,51 @@ class BST:
                     self.splay(new_node)
                     break
 
-    def zig(self, x: Node):
-        print(sys._getframe().f_code.co_name)
+    def splay(self, x_node: Node):
+
+        if not x_node.parent:
+            return
+
+        while x_node.parent:
+            if x_node.parent.parent:
+                if x_node == x_node.parent.left and x_node.parent == x_node.parent.parent.left:
+                    BST.zigzig(x_node)
+                elif x_node == x_node.parent.right and x_node.parent == x_node.parent.parent.right:
+                    BST.zagzag(x_node)
+                elif x_node == x_node.parent.right and x_node.parent == x_node.parent.parent.left:
+                    BST.zigzag(x_node)
+                elif x_node == x_node.parent.left and x_node.parent == x_node.parent.parent.right:
+                    BST.zagzig(x_node)
+            else:
+                if x_node == x_node.parent.left:
+                    BST.zig(x_node)
+                elif x_node == x_node.parent.right:
+                    BST.zag(x_node)
+
+        self.root = x_node
+
+    def search(self, x: Node) -> bool:
+        cur_node = self.root
+        while True:
+            if not cur_node:
+                return False
+            if x == cur_node:
+                self.splay(cur_node)
+                return True
+
+            cur_node = cur_node.left if x < cur_node else cur_node.right
+
+    def very_left(self):
+        if self.root is None:
+            return
+        leftest = self.root
+        while leftest.left:
+            leftest = leftest.left
+
+        return leftest
+
+    @staticmethod
+    def zig(x: Node):
         p = x.parent
         x.parent = p.parent
         p.parent = x
@@ -91,8 +139,8 @@ class BST:
         if x.right: x.right.parent = p
         x.right = p
 
-    def zag(self, x: Node):
-        print(sys._getframe().f_code.co_name)
+    @staticmethod
+    def zag(x: Node):
         p = x.parent
         x.parent = p.parent
         p.parent = x
@@ -100,8 +148,8 @@ class BST:
         if x.left: x.left.parent = p
         x.left = p
 
-    def zigzig(self, x: Node):
-        print(sys._getframe().f_code.co_name)
+    @staticmethod
+    def zigzig(x: Node):
         p = x.parent
         g = p.parent
 
@@ -109,39 +157,21 @@ class BST:
             if g == g.parent.left:
                 g.parent.left = x
             else:
-                g.parent.righ = x
+                g.parent.right = x
         x.parent = g.parent
 
         p.left = x.right
+        if x.right: x.right.parent = p
         x.right = p
         p.parent = x
 
         g.left = p.right
+        if p.right: p.right.parent = g
         p.right = g
         g.parent = p
 
-    def zagzag(self, x: Node):
-        print(sys._getframe().f_code.co_name)
-        p = x.parent
-        g = p.parent
-
-        if g.parent:
-            if g == g.parent.left:
-                g.parent.left = x
-            else:
-                g.parent.righ = x
-        x.parent = g.parent
-
-        p.right = x.left
-        x.left = p
-        p.parent = x
-
-        g.right = p.left
-        p.left = g
-        g.parent = p
-
-    def zigzag(self, x: Node):
-        print(sys._getframe().f_code.co_name)
+    @staticmethod
+    def zagzag(x: Node):
         p = x.parent
         g = p.parent
 
@@ -153,15 +183,17 @@ class BST:
         x.parent = g.parent
 
         p.right = x.left
+        if x.left: x.left.parent = p
         x.left = p
         p.parent = x
 
-        g.left = x.right
-        x.right = g
-        g.parent = x
+        g.right = p.left
+        if p.left: p.left.parent = g
+        p.left = g
+        g.parent = p
 
-    def zagzig(self, x: Node):
-        print(sys._getframe().f_code.co_name)
+    @staticmethod
+    def zigzag(x: Node):
         p = x.parent
         g = p.parent
 
@@ -169,63 +201,54 @@ class BST:
             if g == g.parent.left:
                 g.parent.left = x
             else:
-                g.parent.righ = x
+                g.parent.right = x
         x.parent = g.parent
-        print('g.parent = ', x.parent)
+
+        p.right = x.left
+        if x.left: x.left.parent = p
+        x.left = p
+        p.parent = x
+
+        g.left = x.right
+        if x.right: x.right.parent = g
+        x.right = g
+        g.parent = x
+
+    @staticmethod
+    def zagzig(x: Node):
+        p = x.parent
+        g = p.parent
+
+        if g.parent:
+            if g == g.parent.left:
+                g.parent.left = x
+            else:
+                g.parent.right = x
+        x.parent = g.parent
 
         p.left = x.right
+        if x.right: x.right.parent = p
         x.right = p
         p.parent = x
 
         g.right = x.left
+        if x.left: x.left.parent = g
         x.left = g
         g.parent = x
 
-    def splay(self, x_node: Node):
-        if not x_node.parent:
-            return
 
-        while x_node.parent:
-            if x_node.parent.parent:
-                if x_node == x_node.parent.left and x_node.parent == x_node.parent.parent.left:
-                    self.zigzig(x_node)
-                elif x_node == x_node.parent.right and x_node.parent == x_node.parent.parent.right:
-                    self.zagzag(x_node)
-                elif x_node == x_node.parent.right and x_node.parent == x_node.parent.parent.left:
-                    self.zigzag(x_node)
-                elif x_node == x_node.parent.left and x_node.parent == x_node.parent.parent.right:
-                    self.zagzig(x_node)
-            else:
-                if x_node == x_node.parent.left:
-                    self.zig(x_node)
-                elif x_node == x_node.parent.right:
-                    self.zag(x_node)
-
-        bst.root = x_node
-
-    def search(self, x: Node) -> bool:
-        cur_node = bst.root
-        while True:
-            if not cur_node:
-                return False
-            if x == cur_node:
-                self.splay(cur_node)
-                return True
-
-            cur_node = cur_node.left if x < cur_node else cur_node.right
-
-
-bst = BST(Node(6))
-bst.add(Node(8))
-bst.add(Node(10))
-bst.add(Node(9))
-bst.add(Node(12))
-bst.add(Node(11))
-
-bst.search(Node(8))
-bst.search(Node(10))
-bst.search(Node(11))
-
+from random import randrange
+bst = BST(Node(20))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
+bst.add(Node(randrange(50)))
 
 print(bst)
 
